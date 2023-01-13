a9425a03fbcf22d5c1663c35bb3b8c2a2583d79b
marcelosanchez166 <marcelosanchez166@gmail.com>
2023-01-13 11:23:36 -0600

    entendiendo paquetes Python

---
+++ PotenciaYRedondea/potenciaYredondea.py
@@ -0,0 +1,5 @@
+def potencia(n1,n2):
+          print("la potencia de  ",n1 ,"A",n2, " es: " , n1**n2 )
+
+def redondear(n1):
+          print("se aproximo el numero a : ",round(n1))
---
+++ basicas/Operaciones_basicas.py
@@ -0,0 +1,8 @@
+def sumar(n1,n2):
+          print("El resultado de la suma es: " , n1+n2 )
+
+def restar(n1,n2):
+          print("El resultado de la resta es: ",n1-n2)
+
+def multiplicarr(n1,n2):
+          print("El resultado de la multiplicacion es: ", n1*n2)
